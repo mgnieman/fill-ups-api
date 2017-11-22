102a006ec52c8641aee3d26b31d45bc1b6428913
mgnieman <michael.nieman@libertymutual.com>
2017-11-22 14:20:14 -0500

    Add script for update

---
+++ scripts/fill_ups/update-fill_ups.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl --include --request PATCH "http://localhost:4741/fill_ups/${ID}" \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN" \
+  --data '{
+    "fill_up": {
+      "date": "'"${DATE}"'",
+      "mileage": "'"${MILEAGE}"'",
+      "gallons": "'"${GALLONS}"'",
+      "mpg": "'"${MPG}"'",
+      "price": "'"${PRICE}"'"
+    }
+  }'
