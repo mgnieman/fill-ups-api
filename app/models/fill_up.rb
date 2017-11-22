90d12c5add61c8c2c3962066d89661db4496388e
mgnieman <michael.nieman@libertymutual.com>
2017-11-22 14:47:54 -0500

    Add validation to date field in fill_up model

--- app/models/fill_up.rb
+++ app/models/fill_up.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 class FillUp < ApplicationRecord
   belongs_to :user
+  validates :date, format: { with: /(\d{4})\D?(0[1-9]|1[0-2])\D?([12]\d|0[1-9]|3[01])/, message: "invalid date" }
+  # validates :date, length: { is: 10, message: "invalid date" }
+  # validates :distance, format: { with: /\A\d*\.?\d{1,2}\z/, message: "invalid distance" }
+  # validates :duration, format: { with: /\A\d*\.?\d{1,2}\z/, message: "invalid duration" }
 end
